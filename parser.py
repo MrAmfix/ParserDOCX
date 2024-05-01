9b86f55ce1899ff25e67dbd2f226dedc3dc49884
Inf1nity <fvendetta311@gmail.com>
2024-05-01 05:33:19 +0300

    Added text parsing

--- parser.py
+++ parser.py
@@ -7,13 +7,15 @@ from xml.etree.ElementTree import Element
 from schemas import schemas
 from typing import List, Optional, Dict
 from os.path import join, dirname, abspath
+import logging
 
 
 class Elem:
-    def __init__(self, num: str, text: str):
+    def __init__(self, num: str, text: str, anchor_id: str = None):
         self.num: str = num
         self.text: str = text
         self.sub_elements: Optional[List[Elem]] = None
+        self.anchor_id: str = anchor_id
 
     def append(self, elem):
         if self.sub_elements is None:
@@ -43,6 +45,23 @@ class Parser:
                 for para in content.iter(f'{{{schemas.w}}}p'):
                     paragraphs.append(para)
         return self._parse_paragraphs(paragraphs)
+    
+    def parse_paragraphs_from_anchor(self, anchor_id: str):
+        tree = ET.parse(os.path.join(self._temp_dir, 'word', 'document.xml'))
+        root = tree.getroot()
+
+        check = False
+        text = []
+        for p in root.iter(f"{{{schemas.w}}}p"):
+            if p.findall(f'{{{schemas.w}}}bookmarkStart') and check:
+                return '\n'.join(text)
+            if check:
+                text.append(self._parse_text_from_anchor(p))
+            if p.findall(f'{{{schemas.w}}}bookmarkStart[@{{{schemas.w}}}name="{anchor_id}"]'):
+                check = True
+
+    def _parse_text_from_anchor(self, paragraph: Element):
+        return ''.join([i for i in paragraph.itertext()])
 
     def _parse_paragraphs(self, paragraphs: List[Element]):
         struct = []
@@ -51,37 +70,41 @@ class Parser:
             for pPr in para.iter(f'{{{schemas.w}}}pPr'):
                 for style in pPr.iter(f'{{{schemas.w}}}pStyle'):
                     val = style.attrib[f'{{{schemas.w}}}val']
+                    hyperlink = para.iter(f'{{{schemas.w}}}hyperlink')
+                    hyperlink_id = None
+                    for i in hyperlink:
+                        hyperlink_id = i.attrib[f"{{{schemas.w}}}anchor"]
                     if not val.isdigit():
                         continue
                     elif int(val) // 10 == 1:
                         for text in para.iter(f'{{{schemas.w}}}t'):
-                            struct.append(Elem(str(len(struct) + 1), text.text))
+                            struct.append(Elem(str(len(struct) + 1), text.text, hyperlink_id))
                             break
                     elif int(val) // 10 == 2:
                         for text in para.iter(f'{{{schemas.w}}}t'):
                             if len(struct) == 0:
-                                struct.append(Elem(str(len(struct) + 1), text.text))
+                                struct.append(Elem(str(len(struct) + 1), text.text, hyperlink_id))
                                 potentially_damage = True
                                 break
                             else:
                                 num = f'{str(len(struct))}.{str(len(struct[-1]) + 1)}'
-                                struct[-1].append(Elem(num, text.text))
+                                struct[-1].append(Elem(num, text.text, hyperlink_id))
                                 break
                     elif int(val) // 10 == 3:
                         for text in para.iter(f'{{{schemas.w}}}t'):
                             if len(struct) == 0:
                                 potentially_damage = True
-                                struct.append(Elem(str(len(struct) + 1), text.text))
+                                struct.append(Elem(str(len(struct) + 1), text.text, hyperlink_id))
                                 break
                             elif len(struct[-1]) == 0:
                                 potentially_damage = True
                                 num = f'{str(len(struct))}.{str(len(struct[-1]) + 1)}'
-                                struct[-1].append(Elem(num, text.text))
+                                struct[-1].append(Elem(num, text.text, hyperlink_id))
                                 break
                             else:
                                 num = (f'{str(len(struct))}.{str(len(struct[-1]))}.'
                                        f'{str(len(struct[-1].sub_elements) + 1)}')
-                                struct[-1].sub_elements[-1].append(Elem(num, text.text))
+                                struct[-1].sub_elements[-1].append(Elem(num, text.text, hyperlink_id))
                                 break
         return struct, potentially_damage
 
@@ -98,23 +121,24 @@ class Parser:
         all_text = ''
         for para in root.findall(f'.//{{{schemas.w}}}body//{{{schemas.w}}}p'):
             for t in para.iter(f'{{{schemas.w}}}t'):
-                all_text += f'{t.text}\n'
+                all_text += f'{t.text} '
         return all_text
 
 
-def struct_to_dict(elements: List[Elem]):
+def struct_to_dict(elements: List[Elem], p: Parser):
     if elements is None:
         return
 
-    def convert_element_to_dict(elem: Elem):
+    def convert_element_to_dict(elem: Elem, p: Parser):
         elem_dict = {
             "num": elem.num,
-            "text": elem.text,
+            "title": elem.text,
+            'text': p.parse_paragraphs_from_anchor(elem.anchor_id)
         }
         if elem.sub_elements:
-            elem_dict["sub_elements"] = [convert_element_to_dict(sub_elem) for sub_elem in elem.sub_elements]
+            elem_dict["sub_elements"] = [convert_element_to_dict(sub_elem, p) for sub_elem in elem.sub_elements]
         return elem_dict
-    return [convert_element_to_dict(elem) for elem in elements]
+    return [convert_element_to_dict(elem, p) for elem in elements]
 
 
 def handle_none(obj):
@@ -136,11 +160,12 @@ def parsing_documents():
         try:
             p = Parser(join(root_in, filename))
             s, pot = p.parse()
-            n = {'potentially_damage': pot, 'table_of_content': struct_to_dict(s),
-                 'other_text': p.get_other_text()}
+            n = {'potentially_damage': pot, 'table_of_content': struct_to_dict(s, p),
+                    'other_text': p.get_other_text()}
             p.save(n, join(root_out, filename))
+            print(f"file - {filename} done!")
         except Exception as _e:
-            continue
+            logging.warn(_e)
 
 
 if __name__ == '__main__':
